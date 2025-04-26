038c32f602a5c8fc7cd1849767c7bcd3f65bfc09
wilmer920123 <50815344+wilmer920123@users.noreply.github.com>
2025-04-26 11:41:56 -0500

    a

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: 'https://wilmer920123.github.io/MaquinasVirtuales/',
   plugins: [react()],
 })
